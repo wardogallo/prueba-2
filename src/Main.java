e534d33d4892a14d060a84b8da4e718969225a47
Eugenia <Eugenia@DESKTOP-1DFGGT4>
2018-09-16 14:39:38 +0200

    Versión 1: creación

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hola");
+		
+		int a=0;
+		String var="texto";
+		
+		System.out.println("Probando GIT");
+
+	}
+
+}
